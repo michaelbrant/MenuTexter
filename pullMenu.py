6e7c1cb46aee10df0e769acd9d1fad9229e65e73
michaelbrant <michaeldavidbrant@gmail.com>
2017-04-17 17:04:42 -0400

    Update pullMenu.py

--- pullMenu.py
+++ pullMenu.py
@@ -1,17 +1,15 @@
 import datetime
 moment = datetime.datetime.now()
 
-
+#Get the Current Day, Month, and year
 if (moment.month < 10):
     month  = '0' + str(moment.month)
 else:
     month = str(moment.month)
-
 if (moment.day < 10):
     day  = '0' + str(moment.day)
 else:
     day = str(moment.day)
-
 year = str(moment.year)
 
 #Uncomment out these three lines to test specific days
@@ -28,9 +26,3 @@ def getLunchURL():
 def getDinnerURL():
     url = 'widener.campusdish.com/Commerce/Catalog/Menus.aspx?LocationId=213&PeriodId=1469&MenuDate='+year+'-'+month+'-'+day+'&Mode=day&UIBuildDateFrom=2017-03-13'
     return url
-
-def sendMenu(OneReceiver):
-    url = 'widener.campusdish.com/Commerce/Catalog/Menus.aspx?LocationId=213&PeriodId=1468&MenuDate='+year+'-'+month+'-'+day+'&Mode=day&UIBuildDateFrom=2017-03-13'
-    #myHTMLParser.sendToOne(url, OneReceiver)
-
-
